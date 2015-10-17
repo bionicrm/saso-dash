8143c752244291d88a73d0d8ce07e96377d09d54
T.J. Mazeika <t@mazeika.me>
2015-10-17 14:19:55 -0400

    Move main() to Groovy

---
+++ src/main/groovy/io/saso/dash/Dash.groovy
@@ -0,0 +1,20 @@
+package io.saso.dash
+import com.google.inject.Guice
+import io.saso.dash.modules.*
+import io.saso.dash.server.Server
+
+class Dash
+{
+    static void main(String... args)
+    {
+        final injector = Guice.createInjector new AuthModule(),
+                new ConfigModule(),
+                new DatabaseModule(),
+                new RedisModule(),
+                new ServerModule(),
+                new ServiceModule(),
+                new TemplatingModule()
+
+        injector.getInstance Server start()
+    }
+}
