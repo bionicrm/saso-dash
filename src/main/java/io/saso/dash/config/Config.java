5d190c22c3349aed9ec8881e5146fdf189eac3b9
T.J. Mazeika <t@mazeika.me>
2015-10-27 19:56:02 -0400

    Add docs

--- src/main/java/io/saso/dash/config/Config.java
+++ src/main/java/io/saso/dash/config/Config.java
@@ -2,5 +2,11 @@ package io.saso.dash.config;
 
 public interface Config
 {
+    /**
+     * Gets the config model that holds information about the config. Creates a
+     * new one if it does not yet exist.
+     *
+     * @return the config model
+     */
     ConfigModel getModel();
 }
