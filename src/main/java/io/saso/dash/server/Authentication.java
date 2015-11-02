2d5d7efaaff63c9742bb9b76ee8c8124a54a43a0
T.J. Mazeika <t@mazeika.me>
2015-11-01 22:41:23 -0500

    Whoops we fergot live token validation

--- src/main/java/io/saso/dash/server/Authentication.java
+++ src/main/java/io/saso/dash/server/Authentication.java
@@ -6,5 +6,13 @@ import java.util.Optional;
 
 public interface Authentication
 {
+    /**
+     * Gets a live token from the DB based off the given token. If the token is
+     * invalid or not found, an empty optional is returned.
+     *
+     * @param token the token to authenticate
+     *
+     * @return an optional of the live token; empty if not found or invalid
+     */
     Optional<DBLiveToken> authenticate(String token);
 }
--- src/main/java/io/saso/dash/server/impl/DashAuthentication.java
+++ src/main/java/io/saso/dash/server/impl/DashAuthentication.java
@@ -5,6 +5,8 @@ import io.saso.dash.database.DBFetcher;
 import io.saso.dash.database.entities.DBLiveToken;
 import io.saso.dash.server.Authentication;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Optional;
 
 public class DashAuthentication implements Authentication
@@ -20,6 +22,25 @@ public class DashAuthentication implements Authentication
     @Override
     public Optional<DBLiveToken> authenticate(String token)
     {
-        return dbFetcher.fetch(DBLiveToken.class, "find_live_token", token);
+        Optional<DBLiveToken> liveTokenOptional =
+                dbFetcher.fetch(DBLiveToken.class, "find_live_token", token);
+
+        if (liveTokenOptional.isPresent() && isValid(liveTokenOptional.get())) {
+            return liveTokenOptional;
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Validates the given live token.
+     *
+     * @param liveToken the live token to validate
+     *
+     * @return {@code true} if and only if the live token is valid
+     */
+    private boolean isValid(DBLiveToken liveToken)
+    {
+        return liveToken.getExpiresAt().after(Timestamp.from(Instant.now()));
     }
 }
