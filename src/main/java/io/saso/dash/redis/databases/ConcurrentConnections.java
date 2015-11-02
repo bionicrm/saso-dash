e3c4f2f279d55babe6116132c9797b6b78d42715
T.J. Mazeika <t@mazeika.me>
2015-11-01 22:44:24 -0500

    Document concurrent connections

--- src/main/java/io/saso/dash/redis/databases/ConcurrentConnections.java
+++ src/main/java/io/saso/dash/redis/databases/ConcurrentConnections.java
@@ -2,9 +2,26 @@ package io.saso.dash.redis.databases;
 
 public interface ConcurrentConnections
 {
+    /**
+     * Initializes the concurrent connections Redis DB.
+     */
     void initialize();
 
+    /**
+     * Increments the number of concurrent connections for the given user ID if
+     * allowed. If not allowed, {@code false} will be returned and the number
+     * of concurrent connections for the user ID will remain unchanged.
+     *
+     * @param userId the user ID to increment for
+     *
+     * @return {@code true} if the increment for the user ID is allowed
+     */
     boolean incrementIfAllowed(int userId);
 
+    /**
+     * Decrements the number of concurrent connections for the given user ID.
+     *
+     * @param userId the user ID to decrement for
+     */
     void decrement(int userId);
 }
