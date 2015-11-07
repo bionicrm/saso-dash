076f6713fc431559433593a4cb8316ae5e0aec6b
T.J. Mazeika <t@mazeika.me>
2015-11-07 01:48:13 -0500

    Add TemplateRenderer Javadoc

--- src/main/java/io/saso/dash/templating/TemplateRenderer.java
+++ src/main/java/io/saso/dash/templating/TemplateRenderer.java
@@ -4,5 +4,20 @@ import java.util.Map;
 
 public interface TemplateRenderer
 {
+    /**
+     * Renders the given Twig template using the given model.
+     * {@code templateName} should be a path relative to ./templates that
+     * excludes the file extension. E.g. {@code "path/to/file"} would render
+     * {@code "./templates/path/to/file.twig"}.
+     * <p>
+     * If configured to do so, the templates will be cached. The application
+     * must be restarted for the cache to refresh. If cache is disabled, the
+     * files will be read every time it's rendered.
+     *
+     * @param templateName the name of the template to load
+     * @param model the model to pass to the Twig template
+     *
+     * @return the rendered Twig template
+     */
     String render(String templateName, Map<String, Object> model);
 }
